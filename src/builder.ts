fc9053bdcbe80512edf89e7d59db41312219e81c
Jon Surrell <jon.surrell@automattic.com>
2019-01-11 22:55:04 +0100

    Add build concurrency limit (#82)
    * Add build concurrency limit
    
    * Ensure at least 1 worker

--- src/builder.ts
+++ src/builder.ts
@@ -24,6 +24,8 @@ import { closeLogger, l, getLoggerForBuild } from './logger';
 (git as any).enableThreadSafety();
 
 export const MAX_CONCURRENT_BUILDS = 3;
+export const SINGLE_BUILD_CONCURRENCY = Math.max( 1, Math.floor( os.cpus().length / MAX_CONCURRENT_BUILDS ) );
+
 export const buildQueue: Array<CommitHash> = [];
 const pendingHashes: Set<CommitHash> = new Set();
 
@@ -118,6 +120,7 @@ export async function buildImageForHash(commitHash: CommitHash): Promise<void> {
 			nocache: false,
 			buildargs: {
 				commit_sha: commitHash,
+				workers: SINGLE_BUILD_CONCURRENCY,
 			},
 		});
 	} catch (err) {
