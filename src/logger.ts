69daa4a40212c483c9f0701d4db1fb949a925ca9
Jon Surrell <jon.surrell@automattic.com>
2019-01-15 08:52:01 +0100

    Mock createLogger

--- src/logger.ts
+++ src/logger.ts
@@ -26,8 +26,8 @@ const dserveLogger = bunyan.createLogger( {
 
 /* super convenient name */
 export const l = {
-	log: dserveLogger.info,
-	error: dserveLogger.error,
+	log: dserveLogger.info.bind( dserveLogger ),
+	error: dserveLogger.error.bind( dserveLogger ),
 };
 
 /**
